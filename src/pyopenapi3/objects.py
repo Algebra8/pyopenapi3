51927b1c23f90b38c991f2f9b7ff8b418960fc3d
Milad Nasr <miald@lyte.com>
2021-03-10 12:37:13 -0800

    include Response and RequestBody objects.

--- src/pyopenapi3/objects.py
+++ src/pyopenapi3/objects.py
@@ -1,5 +1,6 @@
-from typing import Iterable, Any
+from typing import Iterable, Any, Optional, Dict, List, Tuple, Union, Type
 import functools
+from dataclasses import dataclass, asdict
 
 
 class OpenApiObject:
@@ -125,3 +126,24 @@ class Reference(Component):
     ...
 
 
+@dataclass
+class Response(OpenApiObject):
+
+    status: int
+    description: Optional[str] = None
+    content: Optional[Dict[str, Any]] = None
+    required: bool = False
+
+    def as_dict(self):
+        return asdict(self)
+
+
+@dataclass
+class RequestBody(OpenApiObject):
+
+    description: Optional[str]
+    content: List[Tuple[str, Type[Union[Field, Component]]]]
+    required: bool = False
+
+    def as_dict(self):
+        return asdict(self)
