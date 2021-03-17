8d12c1fe4c3e368815cb217db54af4c84f6e9ad7
Milad Nasr <miald@lyte.com>
2021-03-17 11:02:46 -0700

    tests for objects to schema and arrays to schemas.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,11 +4,24 @@ from pyopenapi3.objects import (
     Email,
     Int64,
     String,
-    Component
+    Component,
+    Array
 )
-from pyopenapi3.schemas import MediaTypeObject
+from pyopenapi3.schemas import (
+    MediaTypeObject,
+    Int64DTSchema,
+    ReferenceObject,
+    ArrayDTSchema,
+    AnyTypeArrayDTSchema,
+    MixedTypeArrayDTSchema
+)
 from pyopenapi3.types import MediaTypeEnum
-from pyopenapi3.utils import build_mediatype_schema_from_content
+from pyopenapi3.utils import (
+    build_mediatype_schema_from_content,
+    convert_primitive_to_schema,
+    convert_objects_to_schema,
+    convert_array_to_schema
+)
 
 
 def test_build_mediatype_from_object__success():
@@ -51,5 +64,39 @@ def test_build_mediatype_from_tuples__success():
     assert b == should_be
 
 
+def test_convert_primitive_to_schema():
+    p = convert_primitive_to_schema(Int64)
+    assert p == Int64DTSchema()
+
+
+def test_convert_objects_to_schema():
+    class Pet(Component):
+        ...
+
+    r = convert_objects_to_schema(Pet)
+
+    assert r == ReferenceObject(ref='#/components/schemas/Pet')
+
+
+def test_array_to_schema():
+    kwargs = {'min_length': 1, 'max_length': 10}
+
+    arb_array = convert_array_to_schema(Array[...], **kwargs)
+    assert arb_array == AnyTypeArrayDTSchema(**kwargs)
+
+    single_array = convert_array_to_schema(Array[Int64], **kwargs)
+    assert single_array == ArrayDTSchema(
+        items={'type': 'integer', 'format': 'int64'},
+        **kwargs
+    )
+
+    mixed_array = convert_array_to_schema(Array[Int64, Email], **kwargs)
+    assert mixed_array == MixedTypeArrayDTSchema(
+        items={'oneOf': [
+            {'type': 'integer', 'format': 'int64'},
+            {'type': 'string', 'format': 'email'}
+        ]},
+        **kwargs
+    )
 
 
