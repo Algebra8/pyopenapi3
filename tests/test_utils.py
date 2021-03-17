926e9b430b76c76395acc08ad849fe4970fea9da
Milad Nasr <miald@lyte.com>
2021-03-17 09:59:55 -0700

    Testing for build mediatype.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,3 @@
-
 from pyopenapi3.objects import (
     TextPlainMediaType,
     JSONMediaType,
@@ -7,12 +6,13 @@ from pyopenapi3.objects import (
     String,
     Component
 )
+from pyopenapi3.schemas import MediaTypeObject
 from pyopenapi3.types import MediaTypeEnum
 from pyopenapi3.utils import build_mediatype_schema_from_content
 
 
 def test_build_mediatype_from_object__success():
-    class Customer(Component):
+    class Customer(Component):  # Test custom components.
         ...
 
     text = TextPlainMediaType(String)
@@ -20,12 +20,35 @@ def test_build_mediatype_from_object__success():
 
     b = build_mediatype_schema_from_content([text, json])
 
-    assert b == {
-        MediaTypeEnum.JSON: {
-            'schema': {'$ref': '#/components/schemas/Customer'}},
-        MediaTypeEnum.PNG: {
-            'schema': {'type': 'integer', 'format': 'int64'}}
+    should_be = {
+        MediaTypeEnum.JSON: MediaTypeObject(
+            schema={'ref': '#/components/schemas/Customer'}),
+        MediaTypeEnum.PLAIN: MediaTypeObject(
+            schema={'type': 'string'})
     }
+
+    assert b == should_be
+
+
+def test_build_mediatype_from_tuples__success():
+    content = [
+        (MediaTypeEnum.PNG, Int64, None, None, None),
+        (MediaTypeEnum.JSON, String, None, None, None),
+        (MediaTypeEnum.PLAIN, Email, None, None, None)
+    ]
+
+    b = build_mediatype_schema_from_content(content)
+
+    should_be = {
+        MediaTypeEnum.PNG: MediaTypeObject(
+            schema={'type': 'integer', 'format': 'int64'}),
+        MediaTypeEnum.JSON: MediaTypeObject(
+            schema={'type': 'string'}),
+        MediaTypeEnum.PLAIN: MediaTypeObject(
+            schema={'type': 'string', 'format': 'email'})
+    }
+
+    assert b == should_be
 
 
 
