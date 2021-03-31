c0154fee3ba3dbc5ac46b16d1e9aabc8b3093eff
Milad Nasr <miald@lyte.com>
2021-03-30 23:02:38 -0700

    Expose create_schema directly.

--- src/pyopenapi3/__init__.py
+++ src/pyopenapi3/__init__.py
@@ -4,10 +4,13 @@ from .builders import OpenApiBuilder
 import pyopenapi3.objects
 import pyopenapi3.data_types
 
+from pyopenapi3.utils import create_schema
+
 __all__ = (
     "OpenApiBuilder",
     "objects",
     "data_types",
+    "create_schema"
 )
 
 __version__ = "0.1.dev0"
